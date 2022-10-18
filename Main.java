3bdaf758271ef419e8f06e1babd8fe86558c7c56
mandira-05 <113498360+mandira-05@users.noreply.github.com>
2022-10-18 14:50:25 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        float random_coin = (float) Math.random();
-        System.out.println("ans " + random_coin);
-
-    }
-}
